0c59123eb50b2d9cbc069085227104bfb640b8d3
Jacin Fleta de Castro <jacintofleta@gmail.com>
2021-01-24 14:14:27 +0100

    first commit

---
+++ helpers/projects.js
@@ -0,0 +1,80 @@
+const projects = [
+    {
+        title: "Check my Presets",
+        state: "Active",
+        revenue: 800,
+        profitable: "No",
+        description: "Portal donde creadores de contenido de fotografía pueden ganar dinero a través de la venta de presets de lightroom.",
+        launched: "August 2020",
+        timeWorkingOn: "Full time",
+        links: [
+            {
+                name: "Website",
+                url: "https://checkmypresets.com/creator"
+            },
+            {
+                name: "Open metrics",
+                url: "https://checkmypresets.com/open"
+            }
+        ]
+    },
+    {
+        title: "Flamingueo",
+        state: "Sold",
+        soldAmount: 200,
+        profitable: false,
+        link: "https://checkmypresets.com/open",
+        revenue: "Visit link for real time revenue and margin",
+        effort: "8h/day since August 2020",
+    },
+    {
+        title: "Check my Presets",
+        state: "Active",
+        profitable: false,
+        link: "https://checkmypresets.com/open",
+        revenue: "Visit link for real time revenue and margin",
+        effort: "8h/día desde Agosto 2020"
+    },
+    {
+        title: "Check my Presets",
+        state: "Active",
+        profitable: false,
+        link: "https://checkmypresets.com/open",
+        revenue: "Visit link for real time revenue and margin",
+        effort: "8h/día desde Agosto 2020"
+    },
+    {
+        title: "Check my Presets",
+        state: "Active",
+        profitable: false,
+        link: "https://checkmypresets.com/open",
+        revenue: "Visit link for real time revenue and margin",
+        effort: "8h/día desde Agosto 2020"
+    },
+    {
+        title: "Check my Presets",
+        state: "Active",
+        profitable: false,
+        link: "https://checkmypresets.com/open",
+        revenue: "Visit link for real time revenue and margin",
+        effort: "8h/día desde Agosto 2020"
+    },
+    {
+        title: "Check my Presets",
+        state: "Active",
+        profitable: false,
+        link: "https://checkmypresets.com/open",
+        revenue: "Visit link for real time revenue and margin",
+        effort: "8h/día desde Agosto 2020"
+    },
+    {
+        title: "Check my Presets",
+        state: "Active",
+        profitable: false,
+        link: "https://checkmypresets.com/open",
+        revenue: "Visit link for real time revenue and margin",
+        effort: "8h/día desde Agosto 2020"
+    }
+]
+
+export default projects;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,205 @@
+import { useState } from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Container, Heading, Text, Box, Badge, Icon, Flex } from "@chakra-ui/react"
+import projects from '../helpers/projects';
+import { ChevronDownIcon, ChevronUpIcon, LinkIcon } from '@chakra-ui/icons'
+import { IoRocketSharp, IoTime } from 'react-icons/io5';
+import { FaMoneyCheckAlt } from 'react-icons/fa'
 
 export default function Home() {
+
+  const [numberOfProjects , setNumberOfProjects] = useState(5);
+  const [openedProject, setOpenedProject] = useState(null);
+
+  const manageOpenProject = (title) => {
+    console.log("title", title)
+    if(openedProject === title){
+      setOpenedProject(null);
+    } else {
+      setOpenedProject(title);
+    }
+  }
+
+
   return (
-    <div className={styles.container}>
+    <div >
       <Head>
-        <title>Create Next App</title>
+        <title>Jacin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    
+      <Container mt={10} bg={"gray.900"}>
+        <Heading >
+          Hola, soy Jacin
+        </Heading>
+        <Text
+        mt={5}
+        letterSpacing="wide"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          Quiero acabar con la opacidad en el mundo startup. Empezando por mi mismo. Estos son mis proyectos y sus resultados.
+        </Text>
+        {projects.map( (project, index) =>
+        <Box
+          maxW="full"
+          mt={6}
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          key={index}
+          >
+            <Box p={4} mb={1}>
+              {/* TITLE */}
+              <Box
+                  mt="1"
+                  cursor="pointer"
+                  fontWeight="semibold"
+                  as="h4"
+                  lineHeight="tight"
+                  isTruncated
+                  onClick={() => manageOpenProject(project.title)}
+                >
+                  {project.title}
+                  { openedProject === project.title ? <ChevronUpIcon  ml={1}/> : <ChevronDownIcon  ml={1}/> }
+              </Box>
+
+              {/* STATE */}
+              <Box d="flex" mt="2" alignItems="baseline">
+                  {
+                  
+                  project.state === "Sold" ?
+                    <>
+                      <Badge borderRadius="full" px="2" colorScheme="teal">
+                        Sold
+                      </Badge>
+                      <Box
+                        color="gray.400"
+                        fontWeight="semibold"
+                        letterSpacing="wide"
+                        fontSize="xs"
+                        textTransform="uppercase"
+                        ml="2"
+                      >
+                      Sold for €{project.soldAmount}
+                      </Box>
+                    </>
+                  : project.state === "Active" ? 
+                    <>
+                      <Badge borderRadius="full" px="2" colorScheme="teal">
+                        Active
+                      </Badge>
+                        <Box
+                        color="gray.300"
+                        fontWeight="semibold"
+                        letterSpacing="wide"
+                        fontSize="xs"
+                        textTransform="uppercase"
+                        ml="2"
+                      >
+                      Revenue €{project.revenue}/month
+                      </Box>
+                    </>
+                  : 
+                  <Badge borderRadius="full" px="2" colorScheme="red">
+                      Closed
+                  </Badge>
+                  
+                  }
+              </Box>
+
+              {/* VIEW MORE SECTION */}
+              { openedProject === project.title ?
+              <>
+                <Text
+                  mt={4}
+                  mb={4}
+                  color="gray.300"
+                  letterSpacing="wide"
+                  fontSize="md"
+                >
+                  {project.description}
+                </Text>
+
+
+                <Box d="flex" mt="2" alignItems="baseline">
+                  <Badge borderRadius="full" px="2">
+                  <Icon as={IoRocketSharp}  backgroundColor="transparent">
+                  </Icon>
+                  </Badge>
+                  <Box
+                  color="gray.400"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                  >
+                  Launched {project.launched}
+                  </Box>
+                </Box>
+
+                <Box d="flex" mt="2" alignItems="baseline">
+                  <Badge borderRadius="full" px="2">
+                  <Icon as={IoTime}  backgroundColor="transparent">
+                  </Icon>
+                  </Badge>
+                  <Box
+                  color="gray.400"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                  >
+                  Time working on: {project.timeWorkingOn}
+                  </Box>
+                </Box>
+
+                <Box d="flex" mt="2" alignItems="baseline">
+                  <Badge borderRadius="full" px="2">
+                  <Icon as={FaMoneyCheckAlt}  backgroundColor="transparent">
+                  </Icon>
+                  </Badge>
+                  <Box
+                  color="gray.400"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                  >
+                  Profitable: {project.profitable}
+                  </Box>
+                </Box>
+                
+                {project.links ?
+                <>
+                    <Flex mt={4}>
+                    { project.links.map(link =>
+                        <Text as="u" color="gray.400" mr={3}>
+                          <a target="_blank" href={link.url}><LinkIcon /> {link.name}</a>
+                        </Text>
+                    )}
+                    </Flex>
+                </>
+                : null}
+                
+              </>
+
+              : null}
+            </Box>
+        </Box>
+          
+        ).slice(0, numberOfProjects)}
+
+        {numberOfProjects <= projects.length ?
+        <Text align="right" cursor="pointer" mt={3} mb={5} onClick = {() => setNumberOfProjects(numberOfProjects + 3)}>
+          View more
+        </Text>
+        :
+          null
+        }
+
+      </Container>
     </div>
   )
 }
