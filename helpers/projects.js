13355165c40b611fbf16cb7a0f4d3f01870bfb12
Jacin Fleta de Castro <jacintofleta@gmail.com>
2021-04-05 17:40:16 +0200

    Creators Calculator Sold

--- helpers/projects.js
+++ helpers/projects.js
@@ -1,193 +1,200 @@
 const projects = [
-    {
-        title: "Check my Presets",
-        state: "Active",
-        revenue: "Revenue €1500/month",
-        profitable: "No",
-        description: "Portal donde creadores de contenido de fotografía pueden ganar dinero a través de la venta de presets de lightroom.",
-        launched: "August 2020",
-        timeWorkingOn: "Full time",
-        links: [
-            {
-                name: "Website",
-                url: "https://checkmypresets.com/creator"
-            },
-            {
-                name: "Open metrics",
-                url: "https://checkmypresets.com/open"
-            }
-        ]
-    },
-    {
-        title: "Flamingueo",
-        state: "Active",
-        revenue: "Over 100k customers",
-        profitable: "Yes",
-        description: "Marca de referencia para la generación Z en complementos y decoración.",
-        launched: "April 2016",
-        timeWorkingOn: "Full time from 2016 to 2020",
-        links: [
-            {
-                name: "Website",
-                url: "https://flamingueo.com"
-            },
-            {
-                name: "Cofundador Emilio Peña",
-                url: "https://twitter.com/emiliopvb"
-            }
-            ,
-            {
-                name: "Cofundador Pablo Niñoles",
-                url: "https://twitter.com/pniaz"
-            },
-            {
-                name: "Invertido por Angels Capital y Grupo Zriser",
-                url: "https://www.elconfidencial.com/empresas/2019-06-10/jua-roig-serratosa-flamingueo-millenials_2064302/"
-            },
-        ]
-    },
-    {
-        title: "Impuestos.io",
-        state: "Active",
-        revenue: "Revenue €0/month",
-        profitable: "No",
-        description: "Compara cuantos impuestos pagarías en diferentes países. Por ahora solo España y Andorra.",
-        launched: "Jan 2021",
-        timeWorkingOn: "3 hours",
-        links: [
-            {
-                name: "Website",
-                url: "https://impuestos.io"
-            }
-        ]
-    },
-    {
-        title: "My Last Video",
-        state: "Sold",
-        soldAmount: 800,
-        profitable: "No",
-        description: "Introduce tu canal de youtube y obtén un enlace que apunte siempre a tu último video.",
-        launched: "October 2020",
-        timeWorkingOn: "6 hours",
-    },
-    {
-        title: "Creators Calculator",
-        state: "Inactive",
-        revenue: "TOTAL REVENUE €0",
-        profitable: "No",
-        description: "Introduce cuanto dinero quieres ganar y averigua qué tienes que conseguir en cada plataforma",
-        launched: "2020",
-        timeWorkingOn: "8 hours",
-        links: [
-            {
-                name: "Website",
-                url:"https://creatorscalculator.com/"
-            }
-        ]
-    },
-    {
-        title: "Branded Santa",
-        state: "Inactive",
-        revenue: "TOTAL REVENUE €49",
-        profitable: "No",
-        description: "Crea un generador de amigos invisibles de tu marca que recomiende tus productos",
-        launched: "2020",
-        timeWorkingOn: "3 days",
-        links: [
-            {
-                name: "Website",
-                url:"https://brandedsanta.com/"
-            }
-        ]
-    },
-    {
-        title: "Should Learn",
-        state: "Inactive",
-        revenue: "TOTAL REVENUE €0",
-        profitable: "No",
-        description: "Should Learn ordena las habilidades técnicas en función de la oferta laboral",
-        launched: "2020",
-        timeWorkingOn: "5 days",
-        links: [
-            {
-                name: "Website",
-                url:"https://should-learn.herokuapp.com/"
-            }
-        ]
-    },
-    {
-        title: "Topy.co",
-        state: "Closed",
-        revenue: "TOTAL REVENUE €0",
-        profitable: "No",
-        description: "Topy permitía a influencers crearse una lista con sus productos favoritos y automatizar la afiliación.",
-        launched: "2016",
-        timeWorkingOn: "3 months"
-    },
-    {
-        title: "Manifest",
-        state: "Closed",
-        revenue: "TOTAL REVENUE €0",
-        profitable: "No",
-        description: "Añade la bandera del país que quieras a tu foto de perfil.",
-        launched: "2016",
-        timeWorkingOn: "1 week"
-    },
-    {
-        title: "EyL",
-        state: "Sold",
-        soldAmount: 750,
-        profitable: "No",
-        description: "Comunidad de instagram sobre emprendimiento en español.",
-        launched: "2015",
-        timeWorkingOn: "1h/day during 2015",
-    },
-    {
-        title: "Komentu",
-        state: "Closed",
-        revenue: "TOTAL REVENUE €100",
-        profitable: "No",
-        description: "Sistema para aumentar orgánicamente las reviews positivas de restaurantes en TripAdvisor",
-        launched: "2015",
-        timeWorkingOn: "3 months",
-    },
-    {
-        title: "Pikaband",
-        state: "Closed",
-        revenue: "TOTAL REVENUE €10.000",
-        profitable: "No",
-        description: "Pulsera para almacenar información vital como grupo sanguineo o contactos de emergencia con NFC",
-        launched: "2014",
-        timeWorkingOn: "Part time from 2013 to 2015",
-        links: [
-            {
-                name: "Pikaband en TVE",
-                url:"https://www.youtube.com/watch?v=yVhnvPppVOU&ab_channel=Pikaband"
-            }
-        ]
-    },
-    {
-        title: "Versus Company",
-        state: "Closed",
-        revenue: "TOTAL REVENUE €5.000",
-        profitable: "No",
-        description: "Marca de ropa urbana online",
-        launched: "2011",
-        timeWorkingOn: "Part time from 2011 to 2012",
-    },
-    {
-        title: "Tengo Tele",
-        state: "Closed",
-        revenue: "TOTAL REVENUE €4",
-        profitable: "No",
-        description: "Foro donde los usuarios compartian películas.",
-        launched: "2010",
-        timeWorkingOn: "1h/day during 2010",
-    },
-
-
-
-
-]
+  {
+    title: "Check my Presets",
+    state: "Active",
+    revenue: "Revenue €1500/month",
+    profitable: "No",
+    description:
+      "Portal donde creadores de contenido de fotografía pueden ganar dinero a través de la venta de presets de lightroom.",
+    launched: "August 2020",
+    timeWorkingOn: "Full time",
+    links: [
+      {
+        name: "Website",
+        url: "https://checkmypresets.com/creator",
+      },
+      {
+        name: "Open metrics",
+        url: "https://checkmypresets.com/open",
+      },
+    ],
+  },
+  {
+    title: "Flamingueo",
+    state: "Active",
+    revenue: "Over 100k customers",
+    profitable: "Yes",
+    description:
+      "Marca de referencia para la generación Z en complementos y decoración.",
+    launched: "April 2016",
+    timeWorkingOn: "Full time from 2016 to 2020",
+    links: [
+      {
+        name: "Website",
+        url: "https://flamingueo.com",
+      },
+      {
+        name: "Cofundador Emilio Peña",
+        url: "https://twitter.com/emiliopvb",
+      },
+      {
+        name: "Cofundador Pablo Niñoles",
+        url: "https://twitter.com/pniaz",
+      },
+      {
+        name: "Invertido por Angels Capital y Grupo Zriser",
+        url:
+          "https://www.elconfidencial.com/empresas/2019-06-10/jua-roig-serratosa-flamingueo-millenials_2064302/",
+      },
+    ],
+  },
+  {
+    title: "Impuestos.io",
+    state: "Active",
+    revenue: "Revenue €0/month",
+    profitable: "No",
+    description:
+      "Compara cuantos impuestos pagarías en diferentes países. Por ahora solo España y Andorra.",
+    launched: "Jan 2021",
+    timeWorkingOn: "3 hours",
+    links: [
+      {
+        name: "Website",
+        url: "https://impuestos.io",
+      },
+    ],
+  },
+  {
+    title: "My Last Video",
+    state: "Sold",
+    soldAmount: 800,
+    profitable: "No",
+    description:
+      "Introduce tu canal de youtube y obtén un enlace que apunte siempre a tu último video.",
+    launched: "October 2020",
+    timeWorkingOn: "6 hours",
+  },
+  {
+    title: "Creators Calculator",
+    state: "Sold",
+    soldAmount: 300,
+    revenue: "TOTAL REVENUE €0",
+    profitable: "No",
+    description:
+      "Introduce cuanto dinero quieres ganar y averigua qué tienes que conseguir en cada plataforma",
+    launched: "2020",
+    timeWorkingOn: "8 hours",
+    links: [
+      {
+        name: "Website",
+        url: "https://creatorscalculator.com/",
+      },
+    ],
+  },
+  {
+    title: "Branded Santa",
+    state: "Inactive",
+    revenue: "TOTAL REVENUE €49",
+    profitable: "No",
+    description:
+      "Crea un generador de amigos invisibles de tu marca que recomiende tus productos",
+    launched: "2020",
+    timeWorkingOn: "3 days",
+    links: [
+      {
+        name: "Website",
+        url: "https://brandedsanta.com/",
+      },
+    ],
+  },
+  {
+    title: "Should Learn",
+    state: "Inactive",
+    revenue: "TOTAL REVENUE €0",
+    profitable: "No",
+    description:
+      "Should Learn ordena las habilidades técnicas en función de la oferta laboral",
+    launched: "2020",
+    timeWorkingOn: "5 days",
+    links: [
+      {
+        name: "Website",
+        url: "https://should-learn.herokuapp.com/",
+      },
+    ],
+  },
+  {
+    title: "Topy.co",
+    state: "Closed",
+    revenue: "TOTAL REVENUE €0",
+    profitable: "No",
+    description:
+      "Topy permitía a influencers crearse una lista con sus productos favoritos y automatizar la afiliación.",
+    launched: "2016",
+    timeWorkingOn: "3 months",
+  },
+  {
+    title: "Manifest",
+    state: "Closed",
+    revenue: "TOTAL REVENUE €0",
+    profitable: "No",
+    description: "Añade la bandera del país que quieras a tu foto de perfil.",
+    launched: "2016",
+    timeWorkingOn: "1 week",
+  },
+  {
+    title: "EyL",
+    state: "Sold",
+    soldAmount: 750,
+    profitable: "No",
+    description: "Comunidad de instagram sobre emprendimiento en español.",
+    launched: "2015",
+    timeWorkingOn: "1h/day during 2015",
+  },
+  {
+    title: "Komentu",
+    state: "Closed",
+    revenue: "TOTAL REVENUE €100",
+    profitable: "No",
+    description:
+      "Sistema para aumentar orgánicamente las reviews positivas de restaurantes en TripAdvisor",
+    launched: "2015",
+    timeWorkingOn: "3 months",
+  },
+  {
+    title: "Pikaband",
+    state: "Closed",
+    revenue: "TOTAL REVENUE €10.000",
+    profitable: "No",
+    description:
+      "Pulsera para almacenar información vital como grupo sanguineo o contactos de emergencia con NFC",
+    launched: "2014",
+    timeWorkingOn: "Part time from 2013 to 2015",
+    links: [
+      {
+        name: "Pikaband en TVE",
+        url: "https://www.youtube.com/watch?v=yVhnvPppVOU&ab_channel=Pikaband",
+      },
+    ],
+  },
+  {
+    title: "Versus Company",
+    state: "Closed",
+    revenue: "TOTAL REVENUE €5.000",
+    profitable: "No",
+    description: "Marca de ropa urbana online",
+    launched: "2011",
+    timeWorkingOn: "Part time from 2011 to 2012",
+  },
+  {
+    title: "Tengo Tele",
+    state: "Closed",
+    revenue: "TOTAL REVENUE €4",
+    profitable: "No",
+    description: "Foro donde los usuarios compartian películas.",
+    launched: "2010",
+    timeWorkingOn: "1h/day during 2010",
+  },
+];
 
 export default projects;
