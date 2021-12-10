0c999444b95824400cbac9e15fb937b1a66cf92a
Jacin Fleta de Castro <jacintofleta@gmail.com>
2021-12-10 07:29:35 +0100

    Deleted Follow component

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,39 @@
-import Head from 'next/head'
-import { Container, Heading, Text, Box } from "@chakra-ui/react"
-import Follow from '../components/Follow';
-import Projects from '../components/Projects';
+import Head from "next/head";
+import { Container, Heading, Text, Box } from "@chakra-ui/react";
+import Projects from "../components/Projects";
 
 export default function Home() {
-
   return (
-    <div >
+    <div>
       <Head>
         <title>Jacin</title>
         <link rel="icon" href="/favicon.ico" />
         {/* Global site tag (gtag.js) - Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T9J0PV6JR5"></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-T9J0PV6JR5"
+        ></script>
         <script src="/ga.js" async></script>
       </Head>
-    
-      <Container mt={20} bg={"gray.900"}>
-        <Heading >
-          Hola, soy Jacin
-        </Heading>
-        <Text
-        mt={8}
-        letterSpacing="wide"
-        >
-          Quiero acabar con la opacidad en el mundo startup. Empezando por mi mismo. Estos son mis proyectos y sus resultados.
-        </Text>
-      
-        <Projects />
-      
-        <Follow />
 
-        <Box align="center" mb={12}>
-          <Text>Made by <a href="https://twitter.com/jacintofleta" target="_blank"><u>Jacin</u></a></Text>
+      <Container mt={20} bg={"gray.900"}>
+        <Heading>Hola, soy Jacin</Heading>
+        <Text mt={8} letterSpacing="wide">
+          Quiero acabar con la opacidad en el mundo startup. Empezando por mi
+          mismo. Estos son mis proyectos y sus resultados.
+        </Text>
+
+        <Projects />
+
+        <Box align="center" mb={12} mt={10}>
+          <Text>
+            Made by{" "}
+            <a href="https://twitter.com/jacintofleta" target="_blank">
+              <u>Jacin</u>
+            </a>
+          </Text>
         </Box>
-      
       </Container>
     </div>
-  )
+  );
 }
